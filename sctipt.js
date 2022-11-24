83b84784672047172da060d2aaada8455b153b7e
web-developer001 <lochinquvondiqov31@gmail.com>
2022-11-24 21:52:24 +0500

    Davlatlar

--- sctipt.js
+++ sctipt.js
@@ -7,7 +7,12 @@ const baseURL = "https://restcountries.com/v2";
 const getAllCountries = async () => {
   const countries = await fetch(`${baseURL}/all`);
   const result = await countries.json();
-  dataRender(result);
+  $(".wrapper").innerHTML = `<div class="loader"></div>`;
+  setTimeout(() => {
+    $(".wrapper").innerHTML = "";
+    dataRender(result);
+  }, 2000);
+
   diynamicCategory(result);
 };
 
@@ -32,9 +37,11 @@ function dataRender(data = []) {
                 <li class="card-list-item list-unstyled"><strong>Region:</strong> ${e.region}</li>
                 <li class="card-list-item list-unstyled"><strong>Capital: </strong>${e.capital}</li>
               </ul>
+              <button class="btn btn-primary" data-id="${e.name}" >RED MORE</button>
             </div>
    `
     );
+    card.dataset.id = e.name;
 
     $(".wrapper").appendChild(card);
   });
@@ -44,6 +51,7 @@ function dataRender(data = []) {
 // ------------------------------
 function diynamicCategory(data) {
   const category = [];
+
   data.forEach((e) => {
     if (!category.includes(e.region)) {
       category.push(e.region);
@@ -59,8 +67,12 @@ function diynamicCategory(data) {
 }
 
 $("#search").addEventListener("keypress", (e) => {
+  $(".wrapper").innerHTML = `<div class="loader"></div>`;
   if (e.target.value.trim().length !== 0 && e.keyCode === 13) {
-    findCountry(e.target.value);
+    setTimeout(() => {
+      $(".wrapper").innerHTML = "";
+      findCountry(e.target.value);
+    }, 1500);
   }
 });
 
@@ -82,13 +94,17 @@ async function findCountry(country) {
 // ---------------- FIND COUNTRIES END------------------
 
 $("#region").addEventListener("change", (e) => {
-  sortCountry(e.target.value.toLowerCase());
+  $(".wrapper").innerHTML = `<div class="loader"></div>`;
+  setTimeout(() => {
+    $(".wrapper").innerHTML = "";
+    sortCountry(e.target.value.toLowerCase());
+  }, 1500);
 });
 
 async function sortCountry(region) {
   $(".wrapper").innerHTML = "";
 
-  if (region=== "all") {
+  if (region === "all") {
     const response = await fetch(`${baseURL}/all`);
     const data = await response.json();
     if (response.status === 404) {
@@ -113,25 +129,79 @@ async function sortCountry(region) {
   }
 }
 
+$(".wrapper").addEventListener("click", (e) => {
+  $(".country-info").innerHTML = "";
+  if (e.target.classList.contains("btn-primary")) {
+    let id = e.target.getAttribute("data-id");
+    getCountry(id);
+    $(".sidebar").classList.remove("swipe");
+  }
+});
+
+async function getCountry(country) {
+  const response = await fetch(`${baseURL}/name/${country}`);
+  const result = await response.json();
+  console.log(result);
+
+  const {
+    name,
+    capital,
+    region,
+    population,
+    flags: { svg },
+  } = result[0];
+  const row = createElement(
+    "div",
+    "row",
+    `
+  <div class="col-md-4">
+    <img src="${svg}" class="p-5 mt-5" width="420" id="img-country">
+  </div>
+  <div class="col-md-7 p-5 mt-5">
+    <ul class="list-group">
+      <li class="list-group-item" id="cName">Country:${name}</li>
+      <li class="list-group-item">Population: ${population}</li>
+      <li class="list-group-item">Region:${region}</li>
+      <li class="list-group-item">Capital: ${capital}</li>
+    </ul>
+  </div>
+`
+  );
+  $(".country-info").appendChild(row);
+}
+
+$(".close").addEventListener("click", (e) => {
+  $(".sidebar").classList.add("swipe");
+  $("body").style.overflow = "visibel";
+});
+
 // TUNGI REJIM
 
 $(".white").addEventListener("click", () => {
+  // $(".card").forEach(() => {
+  //   $(".card").classList.add("bg-");
+  // });
+
   $(".white").classList.toggle("d-none");
   $(".dark").classList.toggle("d-none");
   $("header").classList.toggle("bg-secondary");
   $("body").setAttribute("class", "text-light bg-dark");
-  $(".card").classList.toggle("bascgrount");
+
   $(".form-control").classList.toggle("bg-secondary");
   $(".form-select").classList.toggle("bg-secondary");
 });
 
 $(".dark").addEventListener("click", () => {
+  // $(".card").forEach(() => {
+  //   $(".card").classList.remove("bg-");
+  // });
+
   $(".white").classList.toggle("d-none");
   $(".dark").classList.toggle("d-none");
   $("body").classList.toggle("bg-dark");
   $("header").classList.toggle("bg-secondary");
   $("body").setAttribute("class", "text-dark ");
-  $(".card").classList.toggle("bascgrount");
+
   $(".form-control").classList.toggle("bg-secondary");
   $(".form-select").classList.toggle("bg-secondary");
 });
